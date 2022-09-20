f8888b16ee16fc72c9b11f3142d20faac4187ee3
JZCS2018 <jingz3017@gmail.com>
2022-09-20 19:55:56 +0000

    update computers

--- src/productbert/train_computers_bert.sh
+++ src/productbert/train_computers_bert.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_small.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-4
+
+python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 5e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 3e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 8e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-4
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 5e-5
@@ -29,55 +15,15 @@
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 8e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-4
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_small_joint.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_large.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 8e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 5e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 3e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 8e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_medium_joint.json --lr 1e-4
+python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 5e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 3e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 8e-5
 python train.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-4
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-5
@@ -89,25 +35,11 @@ python train.py --device 0 -c configs/BERT/computers/config_computers_large_join
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 8e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-4
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_large_joint.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 5e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 3e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 8e-5
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-4
-# python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge.json --lr 1e-4
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 1e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 5e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 8e-5
+
+python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 1e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 5e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 3e-5
+python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 8e-5
 python train.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 1e-4
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 1e-5
 # python train_random.py --device 0 -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 1e-5
