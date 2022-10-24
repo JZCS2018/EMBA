09282198873c0b4f7feef60dad9c6809a14c2a05
JZCS2018 <jingz3017@gmail.com>
2022-10-24 13:47:47 +0000

    update shoes

---
+++ src/productbert/Roberta_train_shoes.sh
@@ -0,0 +1,9 @@
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 1e-4
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 8e-5
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 3e-5
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 1e-5
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 5e-4
+python train.py --device 0 -m Roberta/ -c configs/Roberta/shoes/config_shoes_small.json --lr 8e-4
+
+
+
