cc39fc358d7ff736daa1d029a6b7320b59301f52
Ubuntu <ubuntu@ip-172-31-6-134.us-east-2.compute.internal>
2022-10-26 20:00:31 +0000

    add abl

--- src/productbert/train_all.sh
+++ src/productbert/train_all.sh
@@ -1,39 +1,21 @@
 # #!/bin/bash
 
-# python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_xlarge_joint.json --lr 3e-5
-python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_xlarge_joint.json --lr 5e-6
-python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_xlarge_joint.json --lr 2e-6
+python train.py --device 0 -m ABLavg/ -c configs/BERT/shoes/config_shoes_xlarge_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/shoes/config_shoes_large_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/shoes/config_shoes_medium_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/shoes/config_shoes_small_joint.json --lr 3e-5
 
-# # python train.py --device 0 -c configs/BERT/watches/config_watches_large_joint.json --lr 3e-5
-# # python train.py --device 0 -c configs/BERT/watches/config_watches_large_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_large_joint.json --lr 1e-4
+python train.py --device 0 -m ABLavg/ -c configs/BERT/watches/config_watches_xlarge_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/watches/config_watches_large_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/watches/config_watches_medium_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/watches/config_watches_small_joint.json --lr 3e-5
 
-# # python train.py --device 0 -c configs/BERT/watches/config_watches_xlarge_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_xlarge_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_xlarge_joint.json --lr 1e-4
+python train.py --device 0 -m ABLavg/ -c configs/BERT/computers/config_computers_xlarge_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/computers/config_computers_large_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/computers/config_computers_medium_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/computers/config_computers_small_joint.json --lr 3e-5
 
-# python train.py --device 0 -c configs/BERT/shoes/config_shoes_large_joint.json --lr 3e-5
-python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_large_joint.json --lr 5e-6
-python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_large_joint.json --lr 2e-6
-
-# # python train.py --device 0 -c configs/BERT/watches/config_watches_medium_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_medium_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_medium_joint.json --lr 1e-4
-
-# # python train.py --device 0 -c configs/BERT/watches/config_watches_small_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_small_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/watches/config_watches_small_joint.json --lr 1e-4
-
-# python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_medium_joint.json --lr 8e-5
-python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_medium_joint.json --lr 5e-6
-# python train.py --device 0 -m AOA/ -c configs/BERT/shoes/config_shoes_medium_joint.json --lr 1e-4
-
-# python train.py --device 0 -c configs/BERT/shoes/config_shoes_small_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/shoes/config_shoes_small_joint.json --lr 1e-4
-
-
-# python train.py --device 0 -c configs/BERT/dblpscholar/config_dblpscholar_joint.json --lr 3e-5
-# python train.py --device 0 -c configs/BERT/dblpscholar/config_dblpscholar_joint.json --lr 8e-5
-# python train.py --device 0 -c configs/BERT/dblpscholar/config_dblpscholar_joint.json --lr 1e-4
-
-python train.py --device 0 -m Roberta/ -c configs/Roberta/computers/config_computers_small.json --lr 1e-4
+python train.py --device 0 -m ABLavg/ -c configs/BERT/cameras/config_cameras_xlarge_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/cameras/config_cameras_large_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/cameras/config_cameras_medium_joint.json --lr 3e-5
+python train.py --device 0 -m ABLavg/ -c configs/BERT/cameras/config_cameras_small_joint.json --lr 3e-5
